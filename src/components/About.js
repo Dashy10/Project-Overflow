e8caec5288ac2e337a3aafcd092f23aa705d8c73
Kfrable <kcfrable@gmail.com>
2017-05-23 18:02:42 -0400

    layout added to the about page

--- src/components/About.js
+++ src/components/About.js
@@ -10,8 +10,30 @@ export default class About extends Component {
 
   render(){
   	return(
-  			<div>
-          About component
+  			<div className='flex-wrapper-info'>
+          
+            
+                <h1>Looking For An Answer?</h1> 
+                  <ul>
+                    <li>ldairgq</li>
+                    <li>sarjgkvbqie</li>
+                    <li>krjghqohei</li>
+                    <li>jehrqbfhuerbc</li>
+                    <li>alkjrsbfqle</li>
+                  </ul>
+
+                  <h1>Answering a Question?</h1>
+                  <ul>
+                    <li>jdfgbkuywrb</li>
+                    <li>asakfjgbve</li>
+                    <li>srjglfqeuf</li>
+                    <li>sarhfqeuyrfq</li>
+                    <li>lrgoqfute</li>
+                  </ul>
+            
+
+            
+
   			</div>
   		)
   }
