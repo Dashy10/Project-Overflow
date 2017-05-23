e47190982f13d99719e5a7d6fa714ab37cc7e474
Ed O'Connell <eoconnell44@gmail.com>
2017-05-23 15:37:36 -0400

    added all componenet skeles

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import './App.css';
 import Header from './components/Header'
 import Footer from './components/Footer'
 
+
+const Instructs = () => { return <div style={styles}><p> lorem ipsum ipsum lorem</p></div>}
+const styles = {
+	textAlign: 'center',
+	fontSize: '28'
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
       <Header />
+      <Instructs />
       <Footer />
       </div>
     );
