9b4a28bb3c037a68b46f6b005c9b3a945bc3ffe6
Ed O'Connell <eoconnell44@gmail.com>
2017-05-23 15:02:15 -0400

    footer completed

---
+++ src/components/Footer.js
@@ -0,0 +1,34 @@
+import React,{Component} from 'react';
+import {Bootstrap, Button, DropdownButton, MenuItem, Nav, NavItem, Navbar, NavDropdown, FormGroup, FormControl} from 'react-bootstrap';
+
+const styles = {
+	backgroundColor: 'rgb(26,26,26)',
+	opacity: '.7',
+	color: 'white',
+	height: '100',
+	textAlign: 'center',
+	padding: '2.5% 0',
+	position: 'absolute',
+  right: '0',
+  bottom: '0',
+  left: '0'
+}
+const projTitle = {
+	fontSize: '28'
+}
+
+export default class Footer extends Component {
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+  	return(
+  			<div style={styles}>
+  				<h4 style={projTitle}>ProjectOverflow &copy; 2017</h4> 
+  			</div>
+  		)
+  }
+}
+
+
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,6 @@
 import React,{Component} from 'react';
 import {Bootstrap, Button, DropdownButton, MenuItem, Nav, NavItem, Navbar, NavDropdown, FormGroup, FormControl} from 'react-bootstrap';
 
-
-
-
-
-
-
 class Header extends Component {
   constructor(props){
     super(props)
