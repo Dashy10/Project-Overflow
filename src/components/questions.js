244903655c379476b70972e8aa96e93593e7ac28
Ed O'Connell <eoconnell44@gmail.com>
2017-05-24 16:16:49 -0400

    rendering questions to query page

--- src/components/questions.js
+++ src/components/questions.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import axios from 'axios'
 
 const styles = {
 	margin: '10%'
@@ -7,16 +8,49 @@ const styles = {
 class Questions extends Component{
 	constructor(props){
 		super(props)
+		this.state = {
+			allQuestions: [],
+		}
+	}
+
+	componentWillMount(){
+		this.renderQuestions();
+	}
+
+	renderQuestions(){
+		let url = 'https://project-overflow-db.herokuapp.com/questions';
+
+		axios.get(url)
+		.then((res) => {
+		console.log('do we see data Q--->', res.data.data);
+		let data = res.data.data
+		this.setState({
+			allQuestions: data
+		})
+	})
+}
+
+	renderQ(){
+		console.log('renderQ allQssss', this.state.allQuestions);
+		return this.state.allQuestions.map((e, i) => {
+			console.log('e infoooo', e.question);
+		return (
+			<li> {e.question} </li>
+			)
+		})
+
+			
 	}
 
 	render(){
 		return(
 			<div>
-				loading...
+				loading...	
 				<h1 style={styles}> {this.props.match.params.id} </h1>
-			<p> {console.log('show all props--->', this.props)} </p>
-			<p> {console.log('match params-->', this.props.match.params.id)} </p>
+			<ul>  {this.renderQ()}  </ul>
+					
 				
+
 				{this.props.handleSubmit}
 			</div>
 		)
