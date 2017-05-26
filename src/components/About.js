28ba533a16383f0733f1fcd1e219e0849af537b0
Ed O'Connell <eoconnell44@gmail.com>
2017-05-26 15:00:42 -0400

    about page styling

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,31 @@
 import React, {Component} from 'react';
+import {Image, Grid, Row, Col} from 'react-bootstrap';
+
+const gridInstance = (
+  <Grid>
+    <Row className="show-grid">
+      <Col className='aboutCols' xs={6} md={4} style={{borderRight:'1px solid black'}}> 
+        <img style={{height: '100', width: '100'}} src='https://image.freepik.com/free-icon/question-mark_318-52837.jpg' alt='question mark'></img>
+        <h2 style={{borderBottom: '1px solid black', padding: '1%'}}> How to Search </h2> 
+        <ol>
+          <li> User can enter a topic or keyword in the search bar above </li>
+          <li> CLick on the 'Topics" Dropdown menu and select the desired subject</li>
+          <li> Once on the topics page, all related questions will be displayed </li>
+          <li> User can submit a question which will be posted to the given subjects page </li>
+        </ol>
+      </Col>
+      <Col className='aboutCols' xs={6} md={4}>  
+      <img style={{height: '100', width: '100'}} src='http://img2.wikia.nocookie.net/__cb20130117173952/clubpenguin/images/7/7c/Exclamation_Point_Emoticon.PNG?w=240' alt='exclamation mark'></img>
+      <h2 style={{borderBottom: '1px solid black', padding: '1%'}}> Answering a question </h2> 
+        <ol>
+          <li> While on the topics page user can click the + button to add an answer to a question </li>
+          <li> User may also upvote an answer they see fitting for the question </li>
+          <li> Alternatively, the downvote option is to be used for users' answers that do not correclty answer a questions</li>
+        </ol>
+      </Col>
+    </Row>
+  </Grid>
+  )
 
 class About extends Component {
   constructor(props) {
@@ -8,23 +35,7 @@ class About extends Component {
   render() {
     return (
       <div className='flex-wrapper-info'>
-        <h1>Looking For An Answer?</h1>
-        <ul>
-          <li>ldairgq</li>
-          <li>sarjgkvbqie</li>
-          <li>krjghqohei</li>
-          <li>jehrqbfhuerbc</li>
-          <li>alkjrsbfqle</li>
-        </ul>
-
-        <h1>Answering a Question?</h1>
-        <ul>
-          <li>jdfgbkuywrb</li>
-          <li>asakfjgbve</li>
-          <li>srjglfqeuf</li>
-          <li>sarhfqeuyrfq</li>
-          <li>lrgoqfute</li>
-        </ul>
+        {gridInstance}
 
       </div>
     )
