f3fd5d2bc895179c7f2e45bfc767d784eea4bdd5
Kfrable <kcfrable@gmail.com>
2017-05-29 21:43:11 -0400

    creating new answers

--- src/components/SingleQuestion.js
+++ src/components/SingleQuestion.js
@@ -2,9 +2,19 @@ import React, {Component} from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
+const style ={
+      margin: 'auto',
+    display: 'table'
+}
+
 export default class SingleQuestion extends Component {
   constructor(props){
     super(props)
+    this.state={
+      answer:[],
+      aquestion_id:[],
+      atopic_id:[]
+    }
     // console.log('show props--->', props.match.params.id);
     // console.log('show state--->', props.match.params.subject);
   }
@@ -22,7 +32,7 @@ export default class SingleQuestion extends Component {
       let data = res.data.question;
       let answ = res.data.answer;
       for(let i =0; i<data.length;i++){
-      console.log(data[i].answer);
+      console.log('THIS IS WHAT TO LOOK AT ----->',data[i].question);
       let asked = document.createElement('h1')
       let holder = document.getElementById('holder')
       asked.innerHTML=data[i].question;
@@ -32,6 +42,7 @@ export default class SingleQuestion extends Component {
       // holder.appendChild(li);
       }
       for(let i =0; i<answ.length;i++){
+        console.log('THIS IS WHAT TO LOOK AT ----->',answ);
         let holder = document.getElementById('holder');
         let li = document.createElement('li')
         li.innerHTML=answ[i].answer
@@ -45,13 +56,29 @@ export default class SingleQuestion extends Component {
     //     })
     // })
 
+      answering(){
 
+        let url = 'https://project-overflow-db.herokuapp.com/answers' 
+        let response = document.getElementById('answer').value
+        
+        console.log(response)
+        axios.post(url,{
+      answer:response,
+      /*aquestion_id:,
+      atopic_id:*/
+      
+     })
+      }
 
     render(){
       return(
         <div id='holder'> 
         <h1>hello friend</h1> 
         {this.renderQuestion()}
+        <div style={style}>
+        <input id='answer' type='text' placeholder='Answer'/>
+        <button onClick={this.answering}>Submit</button>
+        </div>
         </div>
         )
     }
